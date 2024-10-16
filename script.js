1aea502b19b1a0b3cde56696cb56a2141c6aa88f
Vbalaji07 <139373829+Vbalaji07@users.noreply.github.com>
2024-10-16 19:59:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var input = document.getElementById('input-box');
+var listContainer = document.getElementById('list-container');
+
+function addTask() {
+    if (input.value === '') {
+        alert("You must Enter Something");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();
+    }
+    input.value ='';
+}
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
